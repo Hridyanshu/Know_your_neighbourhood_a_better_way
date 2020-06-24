1f7078400e07bc0a466e791e5c7e73b59e7accac
Hridyanshu <hridyanshusharma@gmail.com>
2020-06-24 15:00:01 +0530

    Added a feature to open location settings of the user

--- app/lib/screens/login_screen.dart
+++ app/lib/screens/login_screen.dart
@@ -6,7 +6,6 @@ import 'package:knowyourneighbourapp/components/rounded_button.dart';
 import 'package:knowyourneighbourapp/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:knowyourneighbourapp/screens/LocationScreen.dart';
-import 'chat_screen.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 class LoginScreen extends StatefulWidget {
